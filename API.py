1d6873faaee502c667729fc28ee39a37651d4c6d
michalani <michal.anisimow@gmail.com>
2021-04-20 15:37:44 +0100

    API comments

--- API.py
+++ API.py
@@ -12,21 +12,26 @@ boardSize = int(config['DEFAULT']['boardSize'])
 app = Flask(__name__, static_url_path='')
 api = Api(app)
 
+'''
+Load html page for SuDoKu board
+'''
 @app.route("/")
 def hello():
     return render_template('game.html')
 
 '''
 Creates a brand new puzzle when GET requested
+
+http://127.0.0.1:5000/new
+http://127.0.0.1:5000/new?keepCells=40
+keepCells declares how many cells are to be kept as the rest will be purged
 '''
 class setAPuzzle(Resource):
     def get(self):
         board = [[0 for x in range(boardSize)] for y in range(boardSize)]
         setPuzzle(board,0 ,0, True)
         
-        
-        
-        # user input is super evil so we default to a semi-hard puzzle if they try to play with API instead :P
+        # user input is super evil so we default to a puzzle instead of humouring them :P
         try:
             keepCells = int(request.args.get('keepCells'))
             if(keepCells != None and (keepCells > 0) and keepCells < (boardSize*boardSize-1)):
@@ -39,9 +44,12 @@ class setAPuzzle(Resource):
 api.add_resource(setAPuzzle, '/new')
 
 '''
-Since I didn't know if some of the zeros would be cut off I used dots instead '.' of 0's
-solves the puzzle
-#127.0.0.1:5000/solve?puzzle=123456789123456789123456789123456789123456789123456789123456789123456789123456789
+solves the puzzle GET revolsed with one
+
+Why GET and not POST? because javascript is evil.
+
+Since I didn't know if some of the zeros would be cut off I used dots instead of 0's
+http://127.0.0.1:5000/solve?puzzle=....8.9716.34.7852798..134643....5....71..43.5.2349.1.3...1...4..59..163..6.3....
 '''
 class solveAPuzzle(Resource):
     def get(self):
@@ -60,6 +68,14 @@ class solveAPuzzle(Resource):
                 return {'isSolvable': result,'board': outputBoard}
 api.add_resource(solveAPuzzle, '/solve')
 
+'''
+If user has submitted a puzzle we want to know whether it's valid or not this function check and responds with
+isValid:	true
+isValid:	false
+
+http://127.0.0.1:5000/valid?puzzle=254683971613497852798251346431768529967125438582349617329516784845972163176834295
+http://127.0.0.1:5000/valid?puzzle=154683971613497852798251346431768529967125438582349617329516784845972163176834295
+'''
 class isAValidPuzzle(Resource):
     def get(self):
         board = [[0 for x in range(boardSize)] for y in range(boardSize)]        
